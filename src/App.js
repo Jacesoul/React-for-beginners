c3bafb12fc6f2e53f72466cf81cf375d9fa2ea45
Jace <jace@Jaceui-iMac.local>
2021-12-21 18:00:55 +0900

    CleanUp Function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time");
+function Hello() {
+  const effectFn = () => {
+    console.log("Created!!!");
+    return destroyedFn; // Cleanup Function
+  };
+  const destroyedFn = () => {
+    console.log("destroyed :(");
+  };
+  useEffect(effectFn, []);
+  return <h1>Hello</h1>;
+
+  /*
   useEffect(() => {
-    console.log("I run only once");
+    console.log("hello :)");
+    return console.log("bye :("); // Cleanup Function
   }, []);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]); // keyword가 변화할때 코드를 실행할거라고 react.js에게 알려준다.
+  */
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
