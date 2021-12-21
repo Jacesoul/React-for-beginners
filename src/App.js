ff2b793ebe2116b55cc8b88b3aad7337dd518bc2
Jace <jace@Jaceui-iMac.local>
2021-12-21 17:05:02 +0900

    Introduction to EFFECT

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an API");
   return (
     <div>
-      <h1 className={styles.title}>Wecome back!!!</h1>
-      <Button text={"Continue"}></Button>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
