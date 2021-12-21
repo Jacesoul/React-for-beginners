ab8f7578722fd98fd9499245f16968c369923a31
Jace <jace@Jaceui-iMac.local>
2021-12-22 08:57:57 +0900

    decorating Home.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,17 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres, year }) {
   return (
-    <div>
-      <img src={coverImg} alt={title}></img>
-      <h2>
+    <div className={styles.movie}>
+      <img className={styles.movie__img} src={coverImg} alt={title}></img>
+      <h2 className={styles.movie__title}>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
+      <h3 className={styles.movie__year}>{year}</h3>
       <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-      <ul>
+      <ul className={styles.movie__genres}>
         <li>
           {genres.map((genre) => (
             <li key={genre}>{genre}</li>
